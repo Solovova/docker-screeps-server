88e5b06f0ba0c890f26345575a424496e6bf59f4
solovova <vsolo0@gmail.com>
2020-06-14 20:15:40 +0300

    4

--- copy.bat
+++ copy.bat
@@ -1,1 +1,6 @@
-docker run --rm -i -t -v screeps:/from -v docker-screeps-server_screeps:/to alpine ash -c "cd /from ; cp -av . /to"
+SET from1=docker-screeps-server_screeps
+SET from2=docker-screeps-server_mongo-volume
+SET from3=docker-screeps-server_redis-volume
+docker run --rm -i -t --mount type=volume,src=%from1%,dst=/from --mount type=bind,src=c://docker//screeps//archives,dst=/to alpine ash -c "cd /from ; tar cvzf /to/%from1%.tar.gz *"
+docker run --rm -i -t --mount type=volume,src=%from2%,dst=/from --mount type=bind,src=c://docker//screeps//archives,dst=/to alpine ash -c "cd /from ; tar cvzf /to/%from2%.tar.gz *"
+docker run --rm -i -t --mount type=volume,src=%from3%,dst=/from --mount type=bind,src=c://docker//screeps//archives,dst=/to alpine ash -c "cd /from ; tar cvzf /to/%from3%.tar.gz *"
---
+++ restore.bat
@@ -0,0 +1,6 @@
+SET from1=docker-screeps-server_screeps
+SET from2=docker-screeps-server_mongo-volume
+SET from3=docker-screeps-server_redis-volume
+docker run --rm -i -t --mount type=volume,src=%from1%,dst=/to --mount type=bind,src=c://docker//screeps//archives,dst=/from alpine ash -c "cd /to ; rm -rf *; tar xvf /from/%from1%.tar.gz"
+docker run --rm -i -t --mount type=volume,src=%from2%,dst=/to --mount type=bind,src=c://docker//screeps//archives,dst=/from alpine ash -c "cd /to ; rm -rf *; tar xvf /from/%from2%.tar.gz"
+docker run --rm -i -t --mount type=volume,src=%from3%,dst=/to --mount type=bind,src=c://docker//screeps//archives,dst=/from alpine ash -c "cd /to ; rm -rf *; tar xvf /from/%from3%.tar.gz"
